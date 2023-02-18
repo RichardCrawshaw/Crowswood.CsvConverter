bc73e3b7e5c9ab8f551061c4e4f9b8f42d40fa2c
Richard Crawshaw <richardcrawshaw41@gmail.com>
2023-02-18 17:46:28 +1100

    [REFACTOR] Add explicit typeless-data classes
    Add a set of typeless-data classes to simplify handing typeless-data. Base abstract Typeless with generic sub-class, then TypelessNames to hold the field names, TypelessValues to hold a row of values, finally Typelessdata to hold a set of field names and a list of values.
    Refactor the Converter class to use those instead of the explicit string[] types.

---
+++ Crowswood.CsvConverter/Model/TypelessData.cs
@@ -0,0 +1,49 @@
+﻿namespace Crowswood.CsvConverter.Model
+{
+    internal class TypelessData : Typeless<(string[], IEnumerable<string[]>)>
+    {
+        private readonly List<TypelessValues> values = new();
+
+        public TypelessNames Names { get; }
+
+        public IEnumerable<TypelessValues> Values => this.values;
+
+        public string this[int index, string field]
+        {
+            get => this.values[index][this.Names[field]];
+            set => this.values[index][this.Names[field]] = value;
+        }
+
+        public string this[string field, int index]
+        {
+            get => this[index, field];
+            set => this[index, field] = value;
+        }
+
+        public TypelessData(string[] names) => this.Names = new TypelessNames(names);
+
+        public TypelessData(string[] names, IEnumerable<string[]> values)
+            : this(names)
+        {
+            if (values.Any(v => v.Length != names.Length))
+                throw new ArgumentException(
+                    "All values must have the same number of items as the names.",
+                    nameof(values));
+
+            this.values.AddRange(
+                values
+                    .Select(v => new TypelessValues(v)));
+        }
+
+        public void Add(string[] values)
+        {
+            if (values.Length != this.Names.Length)
+                throw new ArgumentException("The number of values must match the number of names.",
+                    nameof(values));
+            this.values.Add(new TypelessValues(values));
+        }
+
+        public override (string[], IEnumerable<string[]>) Get() =>
+            (this.Names.Get(), this.Values.Select(v => v.Get().ToArray()));
+    }
+}
---
+++ Crowswood.CsvConverter/Model/TypelessValues.cs
@@ -0,0 +1,17 @@
+﻿namespace Crowswood.CsvConverter.Model
+{
+    internal class TypelessValues : Typeless<string[]>
+    {
+        private readonly List<string> values = new();
+
+        public string this[int index]
+        {
+            get => values[index];
+            set => values[index] = value;
+        }
+
+        public TypelessValues(IEnumerable<string> values) => this.values.AddRange(values);
+
+        public override string[] Get() => this.values.ToArray();
+    }
+}
